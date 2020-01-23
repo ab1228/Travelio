c3ee06264d73d766885b421ce78152331403a391
ab1228 <alexb8734@gmail.com>
2020-01-22 23:50:18 -0600

    added body parser

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-var app = require('express');
+var express = require('express');
 var exphbs = require('express-handlebars');
+
+var PORT = process.env.PORT || 8001;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
