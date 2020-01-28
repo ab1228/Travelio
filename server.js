58c682c92a2f5cfba20febdc40879b907e2dd290
ab1228 <alexb8734@gmail.com>
2020-01-27 23:39:09 -0600

    added main hbs

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+app.listen(PORT, function () {
+    console.log('App now listening at localhost: ' + PORT);
+});
