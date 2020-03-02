3e911efe123fecf3ecb2f0f13d7a8b280feaa2d9
ab1228 <alexb8734@gmail.com>
2020-03-02 15:13:28 -0600

    updated contact page

--- routes/hotel-api-routes.js
+++ routes/hotel-api-routes.js
@@ -54,6 +54,9 @@ module.exports = function (app) {
     app.get("/sale", function (req, res) {
         res.render("buy");
     });
+    app.get("/contact", function (req, res) {
+        res.render("contact")
+    })
 
 
 
